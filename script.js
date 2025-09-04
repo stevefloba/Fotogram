aa245dd39b4b2316c7a83f122d233d4720e63c4a
Stefan Flosbach <stefanflosbach@t-online.de>
2025-09-04 17:48:11 +0200

    Bildtitel und alt-Tag hinzugefügt und angezeigt

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 const imageFolder = "./assets/img/show/";
 const imageFiles = [
-    "foto0.jpg",
-    "foto1.jpg",
-    "foto2.jpg",
-    "foto3.jpg",
-    "foto4.jpg",
-    "foto5.jpg",
-    "foto6.jpg",
-    "foto7.jpg",
-    "foto8.jpg",
-    "foto9.jpg",
-    "foto10.jpg",
-    "foto11.jpg",
-    "foto12.jpg",
-    "foto13.jpg",
-    "foto14.jpg",
-    "foto15.jpg",
-    "foto16.jpg",
-    "foto17.jpg",
-    "foto18.jpg",
-    "foto19.jpg",
-    "foto20.jpg",
-    "foto21.jpg",
-    "foto22.jpg",
-    "foto23.jpg",
-    "foto24.jpg",
-    "foto25.jpg",
-    "foto26.jpg",
-    "foto27.jpg",
-    "foto28.jpg",
-    "foto29.jpg",
-    "foto30.jpg"
+    { file: "foto0.jpg", title: "smiling dog", alt: "auf dem Rücken liegender lächelnder Hund" },
+    { file: "foto1.jpg", title: "Herr der Ringe Stuntshow, Training", alt: "Steve packt Ben am Hals und hält ihn in der Luft" },
+    { file: "foto2.jpg", title: "Herr der Ringe Stuntshow, Training", alt: "Steve und Ben beim Schwertkampftraining" },
+    { file: "foto3.jpg", title: "Herr der Ringe Stuntshow", alt: "Herr der Ringe Stuntshow. Aragon gegen Lurtz Uruk Hai" },
+    { file: "foto4.jpg", title: "Herr der Ringe Stuntshow", alt: "Herr der Ringe Stuntshow. Aragon gegen Lurtz Uruk Hai" },
+    { file: "foto5.jpg", title: "Herr der Ringe Stuntshow", alt: "Herr der Ringe Stuntshow. Aragon gegen Lurtz Uruk Hai" },
+    { file: "foto6.jpg", title: "Herr der Ringe Stuntshow", alt: "Herr der Ringe Stuntshow. Lawrence Makoare versucht Steve/Lurtz Uruk Hai wieder zubeleben" },
+    { file: "foto7.jpg", title: "Herr der Ringe Stuntshow", alt: "Herr der Ringe Stuntshow. Aragon gegen Lurtz Uruk Hai" },
+    { file: "foto8.jpg", title: "Mortal Combat Stuntshow", alt: "Mortal Combat Stuntshow. Ankunft der Kämpfer auf der Bühne" },
+    { file: "foto9.jpg", title: "Mortal Combat Stuntshow. Backstage", alt: "Mortal Combat Stuntshow. Backstage, Steve mit Girls" },
+    { file: "foto10.jpg", title: "Mortal Combat Stuntshow", alt: "Mortal Combat Stuntshow. Verabschiedung auf der Bühne" },
+    { file: "foto11.jpg", title: "Mortal Combat Stuntshow", alt: "Mortal Combat Stuntshow. Steve wird umgehauen" },
+    { file: "foto12.jpg", title: "Cobra 11, Autobahndreh", alt: "Cobra 11, Autobahndreh. Fliegendes Unfallauto hinter Steve im Flammenmeer" },
+    { file: "foto13.jpg", title: "Cobra 11, Autobahndreh", alt: "Cobra 11, Autobahndreh. Fliegendes Unfallauto hinter Steve im Flammenmeer" },
+    { file: "foto14.jpg", title: "Cobra 11, Autobahndreh", alt: "Cobra 11, Autobahndreh. Fliegendes Unfallauto hinter Steve im Flammenmeer" },
+    { file: "foto15.jpg", title: "Cobra 11, Autobahndreh", alt: "Cobra 11, Autobahndreh. Fliegendes Unfallauto hinter Steve im Flammenmeer" },
+    { file: "foto16.jpg", title: "Cobra 11, Autobahndreh", alt: "Cobra 11, Autobahndreh. Fliegendes Unfallauto hinter Steve im Flammenmeer" },
+    { file: "foto17.jpg", title: "Don 2. The King is Back", alt: "Don 2. The King is Back. Don's Widersacher nnebeneinander kommen auf die Kamera zu" },
+    { file: "foto18.jpg", title: "Don 2. The King is Back", alt: "Don 2. The King is Back. Don's Widersacher nnebeneinander kommen auf die Kamera zu" },
+    { file: "foto19.jpg", title: "Don 2. The King is Back", alt: "Don 2. The King is Back. Don's Widersacher nnebeneinander kommen auf die Kamera zu" },
+    { file: "foto20.jpg", title: "ARD Oskar Kinovorschau", alt: "Steve schlägt golden angemalt einen großen Gong vom ersten Programm" },
+    { file: "foto21.jpg", title: "Steve bringt der Katze das driften bei", alt: "Steve bringt der Katze das driften bei. Steve und Walter im Gespräch" },
+    { file: "foto22.jpg", title: "Steve bringt der Katze das driften bei", alt: "Steve bringt der Katze das driften bei. Steve und Walter posen" },
+    { file: "foto23.jpg", title: "Steve bringt der Katze das driften bei", alt: "Steve bringt der Katze das driften bei. Steve, Walter, D. Katzenberger zeigen Daumen hoch" },
+    { file: "foto24.jpg", title: "Steve bringt der Katze das driften bei", alt: "Steve bringt der Katze das driften bei. Steve mit Katzenberger im Arm" },
+    { file: "foto25.jpg", title: "Ninja-Mama-Dreh", alt: "Ninja-Mama-Dreh. Steve steht mit Pistole und Anzug auf Boot" },
+    { file: "foto26.jpg", title: "Ninja-Mama-Dreh", alt: "Ninja-Mama-Dreh. Landender Hubschrauber auf Rollfeld" },
+    { file: "foto27.jpg", title: "Ninja-Mama-Dreh", alt: "Ninja-Mama-Dreh. Hubschrauber im Anflug über Dühnen" },
+    { file: "foto28.jpg", title: "Steve als Trainer", alt: "Steve als Trainer" },
+    { file: "foto29.jpg", title: "Steve's Setcard", alt: "Steve's Setcard" },
+    { file: "foto30.jpg", title: "Best Friend, Devil", alt: "Steve's Hund Devil auf Hundehütte am relaxen" }
 ];
 
 const gallery = document.querySelector('.gallery');
@@ -52,10 +52,20 @@ function loadImages() {
 
     for (let i = currentIndex; i < end; i++) {
         const img = document.createElement("img");
-        img.src = imageFolder + imageFiles[i];
-        img.alt = imageFiles[i];
+        img.src = imageFolder + imageFiles[i].file;
+        img.alt = imageFiles[i].alt;
+
+        // Bildtitel optional unterhalb anzeigen
+        const figure = document.createElement("figure");
+        const caption = document.createElement("figcaption");
+
+        caption.textContent = imageFiles[i].title;
+
+        figure.appendChild(img);
+        figure.appendChild(caption);
+        gallery.appendChild(figure);
+
         img.addEventListener('click', () => openLightbox(i));
-        gallery.appendChild(img);
         slides.push(img);
     }
 
@@ -77,15 +87,19 @@ loadMoreBtn.addEventListener('click', loadImages);
 // -------------------
 let lightboxIndex = 0;
 
+const lightboxCaption = document.getElementById('lightbox-caption');
+
 function openLightbox(i) {
     lightboxIndex = i;
     lightbox.style.display = 'flex';
-    lightboxImg.src = slides[lightboxIndex].src;
+    lightboxImg.src = imageFolder + imageFiles[i].file;
+    lightboxCaption.textContent = imageFiles[i].title; // Titel anzeigen
 }
 
 function showLightboxSlide(i) {
     lightboxIndex = (i + slides.length) % slides.length;
-    lightboxImg.src = slides[lightboxIndex].src;
+    lightboxImg.src = imageFolder + imageFiles[lightboxIndex].file;
+    lightboxCaption.textContent = imageFiles[lightboxIndex].title;
 }
 
 closeBtn.addEventListener('click', () => {
